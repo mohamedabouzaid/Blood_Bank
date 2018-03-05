e04b3e080160ad7789d38237ec8694878e1f77f1
Mahmoud Samir <mahmoud.samir.fci.scu@gmail.com>
2018-03-05 17:11:02 +0200

    style labTechnicaian

--- view/labTechnician.php
+++ view/labTechnician.php
@@ -21,58 +21,84 @@ if(isset($_SESSION['userName']) && $_SESSION['job']=='lab_Technician'){
 
     ?>
 
-    <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <meta charset="UTF-8">
-        <title>Clinic</title>
-    </head>
 
+<!DOCTYPE html>
 
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clinic</title>
+    <link rel="stylesheet" href="../resource/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../resource/css/Questionnaire.css">
+    <link rel="stylesheet" href="../resource/css/header.css">
+    <link rel="stylesheet" href="../resource/css/Recieptionist.css">
+</head>
 
+<body>
+    
+    <header class="container">
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a href="Home.php" class="navbar-brand" ><img src="../resource/images/logo.png" alt=""></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav links">
+                    <li class="active">
+                         <a href="labTechnician.php" >Whole blood donation</a>     <!-- clinic home button -->
+                    </li>
+                </ul>
+                <!-- search  -->
+                <ul class="nav navbar-nav" style="margin: 9px 112px 0;">
+                    <li>
+                        <form class="form-inline" action="" method="post">
+                            <div class="form-group">
+                                <div class="input-group">
+                                <input class="form-control " type="text" name="search" placeholder="Search..." style="width: 296px">                                     
+                            </div>
+                            <button class="btn btn-danger" type="submit" value="search" style="margin-left: -4px"><span class="glyphicon glyphicon-search"></span></button>
+                            </div>
+                        </form>
+                    </li>
+                </ul>
 
-        <div class="w3-bar w3-light-grey">
-            <a href="labTechnician.php" class="w3-bar-item w3-button">Whole blood donation</a>     <!-- clinic home button -->
-                               <!--search form-->
-            <form action="" method="post">
-                <input type="text" name="search"  class="w3-bar-item w3-input" placeholder="Search..">
-                <input type="submit" value="search"  class="w3-bar-item w3-button w3-blue">
-            </form>
-            <div class="w3-dropdown-hover">
-                                              <!-- user name -->
-                <button class="w3-button"><?php echo $_SESSION['userName'] ?></button>
-<div class="w3-dropdown-content w3-bar-block w3-card-4">
-    <a href="../controller/user/Logoutcontroller.php" class="w3-bar-item w3-button"> logout</a>   <!-- logout button -->
-</div>
-</div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                   <span class="name"> <?php echo $_SESSION['userName'] ?></span>
+                </li>
+                <li><a class="btn" id="login" href="../controller/user/Logoutcontroller.php">Logout</a> </li>       <!-- login button-->
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </header>
 
-
-</div>
+        <img src="../resource/images/3.jpg" alt="" class="bg-image" style="margin-top:7px">
+        <div class="blure-body" style="background: linear-gradient(to left, #ffffff00 ,#f85f65b8);"></div>
 
     <?php
     //table of user
     if($users!= null)
     {
-        echo '<table  class="w3-table w3-striped">
-                                          <caption >Users</caption>
-                                          <tr class="w3-blue">
-                                              <th>رقم السجل القومى /الاقامه</th>
-                                              <th> Health Information</th>
-                                              <th>Modife</th>
-                                          </tr>';
+        echo '<div class="col-md-10 col-md-offset-1"  id="container-table" style="background: #e87e79c7;">
+                <table  class="table table-hover">
+                        <caption >Users</caption>
+                            <tr class="w3-blue">
+                                <th>رقم السجل القومى /الاقامه</th>
+                                <th> Health Information</th>
+                                <th>Modife</th>
+                            </tr>';
         foreach ($users as $user) {
 
-            echo "   <tr>
-                                                <td>" . $user['donar_NID'] . "</td>
-                                                 <td><a  class=\"w3-btn w3-gray\" href='labTechnicianOperation.php ?do=insert& nid=" . $user['donar_NID'] . "'>insert</a></td>
-                                                 <td><a  class=\"w3-btn w3-gray\" href='labTechnicianOperation.php ?do=update& nid=" . $user['donar_NID'] . "'>update</a></td>
-                                                </tr>";
+            echo "          <tr>
+                                <td>" . $user['donar_NID'] . "</td>
+                                <td><a  class=\"btn btn-default\" href='labTechnicianOperation.php ?do=insert& nid=" . $user['donar_NID'] . "'>Insert <span class='glyphicon glyphicon-user'></span></a></td>
+                                <td><a  class=\"btn btn-default\" href='labTechnicianOperation.php ?do=update& nid=" . $user['donar_NID'] . "'>Update <span class='glyphicon glyphicon-edit'></span></a></td>
+                            </tr>";
 
         }
-        echo    "</table>";
+        echo    "</table> </div>";
 
         if(isset($_SESSION['labOperation'])&& $_SESSION['labOperation']!=null){
             echo $_SESSION['labOperation'];
