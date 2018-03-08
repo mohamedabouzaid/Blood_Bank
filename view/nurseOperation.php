7f3306bb46e9963bb32718d8c9bb0b572907a09d
abouzaid <mohmmdabouzaid@gmail.com>
2018-03-09 00:55:50 +0200

    add stop watch

--- view/nurseOperation.php
+++ view/nurseOperation.php
@@ -8,7 +8,8 @@ if(isset($_SESSION['userName']) && $_SESSION['job']=='Nurse') {
 
     if (isset($_POST['insert'])) {
 //the comment
-
+print_r($_POST);
+die();
 
         if (!empty($_POST['comments_list'])) {
             $comment = implode('-', $_POST['comments_list']);
@@ -113,7 +114,16 @@ if(isset($_SESSION['userName']) && $_SESSION['job']=='Nurse') {
             <h3>Blood sample of <?php echo $_GET['nid']; ?></h3>
             <!--form of blood information-->
             <form action="" method="post">
+<!--      stop watch          -->
+                <nav class="controls">
+                    <a href="#" class="button" onClick="stopwatch.start();">Start</a>
+                    <a href="#" class="button" onClick="stopwatch.lap();">Get Time</a>
+                    <a href="#" class="button" onClick="stopwatch.stop();">Stop</a>
+                    <div class="stopwatch"></div>
 
+                </nav>
+                Time of process:<ul class="results"></ul>
+                <input type="hidden" value="" name="test">
                 <input type="radio" name="NID" required <?php if(isset($edit)){ echo"checked" ;}?>>Checked ID<br>
                 Sealed by ID<input type="number" name="ID_Blood" <?php if(isset($edit)){ echo "value='".$edit['ID']."'" ;}?>><br>
                 Bag Weight <input type="number" name="bagWeight"<?php if(isset($edit)){ echo "value='".$edit['bagWeight']."'" ;}?>><br>
@@ -166,8 +176,112 @@ if(isset($_SESSION['userName']) && $_SESSION['job']=='Nurse') {
             </form>
 
 
+        <script>
 
 
+
+
+            class Stopwatch {
+                constructor(display, results) {
+                    this.running = false;
+                    this.display = display;
+                    this.results = results;
+                    this.laps = [];
+                    this.reset();
+                    this.print(this.times);
+                }
+
+                reset() {
+                    this.times = [ 0, 0, 0 ];
+                }
+
+                start() {
+                    if (!this.time) this.time = performance.now();
+                    if (!this.running) {
+                        this.running = true;
+                        requestAnimationFrame(this.step.bind(this));
+                    }
+                }
+
+                lap() {
+                    let times = this.times;
+                    let li = document.createElement('li');
+                    li.innerText = this.format(times);
+                    this.results.appendChild(li);
+                }
+
+                stop() {
+                    this.running = false;
+                    this.time = null;
+                }
+
+                restart() {
+                    if (!this.time) this.time = performance.now();
+                    if (!this.running) {
+                        this.running = true;
+                        requestAnimationFrame(this.step.bind(this));
+                    }
+                    this.reset();
+                }
+
+                clear() {
+                    clearChildren(this.results);
+                }
+
+                step(timestamp) {
+                    if (!this.running) return;
+                    this.calculate(timestamp);
+                    this.time = timestamp;
+                    this.print();
+                    requestAnimationFrame(this.step.bind(this));
+                }
+
+                calculate(timestamp) {
+                    var diff = timestamp - this.time;
+                    // Hundredths of a second are 100 ms
+                    this.times[2] += diff / 10;
+                    // Seconds are 100 hundredths of a second
+                    if (this.times[2] >= 100) {
+                        this.times[1] += 1;
+                        this.times[2] -= 100;
+                    }
+                    // Minutes are 60 seconds
+                    if (this.times[1] >= 60) {
+                        this.times[0] += 1;
+                        this.times[1] -= 60;
+                    }
+                }
+
+                print() {
+                    this.display.innerText = this.format(this.times);
+                }
+
+                format(times) {
+                    return `\
+        ${pad0(times[0], 2)}:\
+        ${pad0(times[1], 2)}:\
+        ${pad0(Math.floor(times[2]), 2)}`;
+                }
+            }
+
+            function pad0(value, count) {
+                var result = value.toString();
+                for (; result.length < count; --count)
+                    result = '0' + result;
+                return result;
+            }
+
+            function clearChildren(node) {
+                while (node.lastChild)
+                    node.removeChild(node.lastChild);
+            }
+
+            let stopwatch = new Stopwatch(
+                document.querySelector('.stopwatch'),
+                document.querySelector('.results'));
+
+
+        </script>
 
 
 
